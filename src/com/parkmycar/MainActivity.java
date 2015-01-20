e239421aed0c826516ad001f0a081df93cdede60
vijayaammineni <vijaya.ammineni09@gmail.com>
2015-01-20 14:22:14 -0800

    Added Search GUI element

--- src/com/parkmycar/MainActivity.java
+++ src/com/parkmycar/MainActivity.java
@@ -1,18 +1,20 @@
 package com.parkmycar;
 
+import android.app.SearchManager;
 import android.content.Context;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 
 
@@ -47,5 +49,19 @@ public class MainActivity extends ActionBarActivity {
         LocationUtils.addCarMarker(googleMap,currentLocation);
 	 }
 	
-	
+	 @Override
+	 public boolean onCreateOptionsMenu(Menu menu) {
+	     MenuInflater inflater = getMenuInflater();
+	     inflater.inflate(R.menu.options_menu, menu);
+	     
+	  // Associate searchable configuration with the SearchView
+	     SearchManager searchManager =
+	            (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+	     SearchView searchView =
+	             (SearchView) menu.findItem(R.id.search).getActionView();
+	     
+	     searchView.setSearchableInfo(
+	             searchManager.getSearchableInfo(getComponentName()));
+	     return true;
+	 }
 }
